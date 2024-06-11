a432cdb241884810655ae68003bd6de9a6200f38
sheedh <156153162+mohamedmahroofa@users.noreply.github.com>
2024-06-11 17:24:09 -0600

    Added some basic wishlist with checkboxes, will continue this

--- Wish-List/src/app/app.component.ts
+++ Wish-List/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from '../shared/models/wishitem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  items : WishItem[] = [
+    new WishItem("To Lean Angular"),
+    new WishItem("To be a good developer", true),
+    new WishItem("To be a filthy rich")
+  ];
+
   title = 'Wish-List';
 }
