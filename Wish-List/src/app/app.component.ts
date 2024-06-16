44c50d5c22a85f99f50be97116609003432f62cf
sheedh <156153162+mohamedmahroofa@users.noreply.github.com>
2024-06-15 22:08:01 -0600

    Added the filtered option to the wish list

--- Wish-List/src/app/app.component.ts
+++ Wish-List/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { WishItem } from '../shared/models/wishitem';
+import { concatWith } from 'rxjs';
+
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+]
 
 @Component({
   selector: 'app-root',
@@ -9,20 +16,26 @@ import { WishItem } from '../shared/models/wishitem';
 export class AppComponent {
 
   items : WishItem[] = [
-    // new WishItem("To Lean Angular"),
-    // new WishItem("To be a good developer", true),
-    // new WishItem("To be a filthy rich")
+    new WishItem("To Lean Angular."),
+    new WishItem("To be a good developer.", true),
+    new WishItem("To be a filthy rich.")
   ];
 
-  newWishText = '';
+  fileredList : any = '0';
+
+  newWishText : string = '';
 
   title = 'Wish-List';
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.fileredList]);
+  }
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
   }
-
+  
   toggleItem(item : WishItem) {
     item.isComplete = !item.isComplete;
     console.log(item);
