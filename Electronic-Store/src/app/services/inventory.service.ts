537dc41a4e59e5991db05b8fa01cd9fee63d4fa0
sheedh <156153162+mohamedmahroofa@users.noreply.github.com>
2024-07-14 03:20:39 -0600

    Added Inventory service in service folder

---
+++ Electronic-Store/src/app/services/inventory.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
+import { Inventory } from '../models/inventory.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+  private url = 'http://localhost:3000/inventories'; // URL to the JSON server 
+
+  constructor(private http: HttpClient) { }
+
+  // Fetch all the products in the inventory
+  getInventories(): Observable<Inventory[]> {
+    return this.http.get<Inventory[]>(this.url);
+  }
+
+  // Fetch a single product in the inventory by id
+  getInventory(id: string): Observable<Inventory> {
+    return this.http.get<Inventory>(`${this.url}/${id}`);
+  }
+
+  // Add a new product to the inventory and also generate a unique id for the product
+  addInventory(inventory: Inventory): Observable<Inventory> {
+    inventory.id = uuidv4();
+    return this.http.post<Inventory>(this.url, inventory);
+  }
+
+  // Update an existing product in the inventory
+  updateInventory(inventory: Inventory): Observable<Inventory> {
+    return this.http.put<Inventory>(`${this.url}/${inventory.id}`, inventory);
+  }
+
+  // Delete a product in the inventory by id
+  deleteInventory(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+}
