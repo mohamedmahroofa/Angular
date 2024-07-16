72b0a80850fdb8731044972b6e5d290b73927c75
sheedh <156153162+mohamedmahroofa@users.noreply.github.com>
2024-07-16 04:31:21 -0600

    Added Navigation Component and Customized the nav.html to show the first display

--- Electronic-Store/src/app/app.module.ts
+++ Electronic-Store/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
