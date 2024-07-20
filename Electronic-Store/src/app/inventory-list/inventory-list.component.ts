bded3896b097ed9b97b6b153cb7b441b8c16eb43
sheedh <156153162+mohamedmahroofa@users.noreply.github.com>
2024-07-19 22:38:34 -0600

    Updated inventory-list component and add the display on inventory-list html

--- Electronic-Store/src/app/inventory-list/inventory-list.component.ts
+++ Electronic-Store/src/app/inventory-list/inventory-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../services/inventory.service';
+import { Inventory } from '../models/inventory.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inventory-list',
   templateUrl: './inventory-list.component.html',
   styleUrl: './inventory-list.component.scss'
 })
-export class InventoryListComponent {
+export class InventoryListComponent implements OnInit{
+  inventories: Inventory[] = [];
 
+  constructor(private inventoryService: InventoryService, private router: Router) {}
+
+  ngOnInit() {
+    // Fetch Products in the Inventory on inventory-list component initialization
+    this.inventoryService.getInventories().subscribe((data) => {
+      this.inventories = data;
+    });
+  }
+
+  // Navigate to the Product details on the inventory component
+  viewInventory(inventoryId: string) {
+    this.router.navigate(['/inventory', inventoryId])
+  }
+
+  // Navigate to the new Product form on the inventory component
+  addInventory() {
+    this.router.navigate(['/inventory']);
+  }
 }
