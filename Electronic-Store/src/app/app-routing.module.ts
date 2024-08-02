546b67d7f9a0df4411f1195f176095dc7a62f17f
sheedh <156153162+mohamedmahroofa@users.noreply.github.com>
2024-08-02 03:54:39 -0600

    Updated the app-routing.module.ts for navigation

--- Electronic-Store/src/app/app-routing.module.ts
+++ Electronic-Store/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InventoryListComponent } from './inventory-list/inventory-list.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inventories', component: InventoryListComponent },
+  { path: 'inventory/:id', component: InventoryComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: '', redirectTo: 'inventories', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
